7f7d9d9dcccbe6324ca8a9fbca1fe355bf80c169
maxbachmann <44199644+maxbachmann@users.noreply.github.com>
2019-10-31 14:41:06 +0100

    replace deprecated stuff

--- action-maxbachmann-GutenMorgen.py
+++ action-maxbachmann-GutenMorgen.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 import configparser
 import io
@@ -10,7 +9,7 @@ import json
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
-class SnipsConfigParser(configparser.SafeConfigParser):
+class SnipsConfigParser(configparser.ConfigParser):
     def to_dict(self):
         return {section: {option_name: option for option_name, option in self.items(section)} for section in self.sections()}
 
@@ -18,7 +17,7 @@ def read_configuration_file(configuration_file):
     try:
         with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
             conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
+            conf_parser.read_file(f)
             return conf_parser.to_dict()
     except (IOError, configparser.Error) as e:
         return dict()
